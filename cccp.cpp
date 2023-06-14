18b800e19fffac30720eba196e39c1bed4edad5e
Kuvrot <jamessanz@outlook.com>
2023-06-13 19:33:54 -0600

    Add files via upload

--- cccp.cpp
+++ cccp.cpp
@@ -6,20 +6,23 @@
 
 //This is memory variable will store all the variables declared
 std::vector <int> memory;
+std::vector <int> methods;
 std::vector <std::string> IDs;
+std::vector <std::string> Method_IDs;
 std::fstream file;
 std::string path;
-std::string keywords = "comrade if -> + - = / * true false manifesto propaganda ; 1 2 3 4 5 6 7 8 9 0 send worker";
+std::string keywords = "comrade if -> + - = / * true false manifesto propaganda ; 1 2 3 4 5 6 7 8 9 0 send worker alert += -+ /= *= contribute gulag";
 std::vector <int> sum;
 
 void compile();
-void run();
 void createIntVariable(std::string name, int data);
-int getIntVariable(std::string name);
+int  getIntVariable(std::string name);
+int  methodMath(int operationID, int previousValue, int nextValue);
 void intMath (int operationID , std::string name, int addValue);
 void setIntVariable(std::string name, int data);
-bool isKeyword(std::string name);
-bool isNumber (std::string data);
+void setMethod(std::string, int data);
+bool isKeyword (std::string name);
+bool isNumber  (std::string data);
 
 std::string errors(int ID , std::string variableName);
 
@@ -131,6 +134,51 @@ void compile() {
             
                 }
 
+                if (words[i] == "-=") {
+
+                    if (isNumber(words[i + 1])) {
+
+                        intMath(1, words[i - 1], std::stoi(words[i + 1]));
+
+                    }
+                    else {
+
+                        intMath(1, words[i - 1], getIntVariable(words[i + 1]));
+
+                    }
+
+                }
+
+                if (words[i] == "*=") {
+
+                    if (isNumber(words[i + 1])) {
+
+                        intMath(2, words[i - 1], std::stoi(words[i + 1]));
+
+                    }
+                    else {
+
+                        intMath(2, words[i - 1], getIntVariable(words[i + 1]));
+
+                    }
+
+                }
+
+                if (words[i] == "/=") {
+
+                    if (isNumber(words[i + 1])) {
+
+                        intMath(3, words[i - 1], std::stoi(words[i + 1]));
+
+                    }
+                    else {
+
+                        intMath(3, words[i - 1], getIntVariable(words[i + 1]));
+
+                    }
+
+                }
+
                 //Logical operators
 
 
@@ -176,7 +224,97 @@ void compile() {
 
                 //Use contribute as a name for a function for user input 
 
-                //methods         
+                if (words[i] == "contribute") {
+                    
+                    int t = 0;
+                    std::cin >> t;
+
+                    if (words[i + 2] != ")" && words[i + 2] != "(" && words[i + 2] != "()") {
+                    
+                        setIntVariable(words[i + 2], t);
+
+                    }
+
+                    
+                    
+                
+                }
+
+                //methods     
+
+                if (words[i] == "worker") {
+                
+                    try {
+
+                        if (!isKeyword(words[i + 1])) {
+
+                                methods.push_back(0);
+                                IDs.push_back(words[i+1]);
+
+                                int r = 0;
+                                int previusValue = 0;
+                                int nextValue = 0;
+
+                                if (isNumber(words[i + 3])) {
+
+                                    previusValue = std::stoi(words[i + 3]);
+
+                                }
+                                else {
+
+                                    previusValue = getIntVariable(words[i + 3]);
+
+                                }
+
+                                if (isNumber(words[i + 7])) {
+
+                                    previusValue = std::stoi(words[i + 5]);
+
+                                }
+                                else {
+
+                                    previusValue = getIntVariable(words[i + 5]);
+
+                                }
+
+                                if (words[i + 4] == "+") {
+
+                                    r = methodMath(0, previusValue, nextValue);
+
+                                }
+                                else if (words[i + 4] == "-") {
+
+                                    r = methodMath(1, previusValue, nextValue);
+
+                                }
+                                else if (words[i + 4] == "*") {
+
+                                    r = methodMath(2, previusValue, nextValue);
+
+                                }
+                                else if (words[i + 4] == "/") {
+
+                                    r = methodMath(3, previusValue, nextValue);
+
+                                }
+
+                                setMethod(words[i + 1], r);
+
+                        }
+                        else {
+
+                            break;
+                        }
+
+                    }
+                    catch (std::exception& e) {
+
+                        std::cout << errors(1001, words[i + 1]) << "An error ocurred" << e.what();
+
+                    }
+                
+                }
+
             }
 
             code += sa;
@@ -185,15 +323,6 @@ void compile() {
         
   
     file.close();
-
-    run();
-
-}
-
-
-void run() {
-
-
 
 }
 
@@ -251,10 +380,6 @@ bool isNumber(std::string data) {
 //All this methods are for manipulating int variables
 
 void createIntVariable(std::string name , int data) {
-
-    std::string sentence = keywords;
-    std::istringstream iss(sentence);
-    std::string word;
 
         try {
 
@@ -316,6 +441,46 @@ void intMath (int operationID, std::string name, int addValue) {
 
 }
 
+//The methods down below are for creation and manipulation of methods or functions
+
+void setMethod(std::string name, int data) {
+
+    for (unsigned int i = 0; i < Method_IDs.size(); i++) {
+
+        if (Method_IDs[i] == name) {
+
+            try {
+
+                methods[i] = data;
+
+            }
+            catch (std::exception& e) {
+
+                std::cout << "comrade " + name + " can't be equal to " << data << e.what() << std::endl;
+
+            }
+        }
+    }
+
+}
+
+int methodMath(int operationID, int previousValue, int nextValue) {
+
+    switch (operationID) {
+    
+    case 0: return previousValue + nextValue; break;
+    case 1: return previousValue - nextValue; break;
+    case 2: return previousValue * nextValue; break;
+    case 3: return previousValue / nextValue; break;
+    
+    
+    }
+
+
+
+}
+
+
 //This method returns a string with an error message depending of the given ID , the string variableName is for errors that need to be more specific but if is not you can you pass "" in that field
 
 std::string errors(int ID , std::string variableName) {
@@ -323,6 +488,7 @@ std::string errors(int ID , std::string variableName) {
     switch (ID) {
         
         case 1000: return "Error 1000: Ilegal name '" + variableName + "'" + " for comrade"; break;
+        case 1001: return "Error 1000: Ilegal name '" + variableName + "'" + " for worker"; break;
         case 1917: return "Error 1917: File Liberation Unsuccessful!, The requested file is missing in action."; break;
         case 2000: return "Error 2000: socialist print syntax error."; break;
     
